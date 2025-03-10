5c1db64b67e8198c7f4db3b65ed1c0b226fac67f
sakthivel182006 <sakthivelv202222@gmail.com>
2025-03-10 13:28:48 +0530

    website complete

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "https://sakthibackend-rhza.vercel.app" }));
+app.use(cors({ origin: "https://sivanethunaitoursandtravels.vercel.app" }));
 
 
 app.get("/", (req, res) => {
