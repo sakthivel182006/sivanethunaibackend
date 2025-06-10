3982486990a847c66dd842fb6028ae09e888d3e3
sakthivel182006 <156519241+sakthivel182006@users.noreply.github.com>
2025-06-11 00:23:39 +0530

    Update booking.model.js

--- models/booking.model.js
+++ models/booking.model.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const bookingSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   from: { type: String, required: true },
-  date: { type: Date, required: true },
+  date: { type: String, required: true },
   destination: { type: String, required: true },
   phone: { type: String, required: true },
-  members: { type: Number, required: true },
+  members: { type: String, required: true },
 });
 
 const Booking = mongoose.model("Booking", bookingSchema);
